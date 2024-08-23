da910daf5ed4ac73e3f2ab0143459a7d25cb3abd
akbaraditamasp <akbaraditamasp@gmail.com>
2024-08-23 14:55:45 +0700

    fix: update new clients

--- examples/client.ts
+++ examples/client.ts
@@ -1,29 +1,9 @@
 import waClient from "../src/index";
-import { StatusEvent } from "../src/types/Events";
 
 (async () => {
-  const client = await waClient("localhost", 3500).create();
+  const client = await waClient("localhost", 5000).create();
 
-  client.start({ id: "default" });
-
-  client.on("status", (e: StatusEvent) => console.log(e));
-  client.on("message.sent", (e: MessageEvent) => console.log(e));
-  client.on("message.fail", (e: MessageEvent) => console.log(e));
-
-  setTimeout(() => {
-    (async () => {
-      for (let index = 0; index < 25; index++) {
-        await client.send({
-          id: "default",
-          to: "6283836319218",
-          content: {
-            text: "Hello",
-          },
-          extraParams: {
-            key: index,
-          },
-        });
-      }
-    })();
-  }, 5000);
+  client.on("connected", () => {
+    client.start({ id: "videa-blast" });
+  });
 })();
